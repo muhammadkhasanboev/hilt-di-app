bb9748b934ce5b8a132fb31a4539e79119f33014
Muhammad Khasanboev <m.khasanboev@tadi.uz>
2026-05-25 14:17:32 +0500

    httpclient, moshi, retrofit added

--- app/src/main/java/uz/muhammad/hilt_di_app/di/NetworkModule.kt
+++ app/src/main/java/uz/muhammad/hilt_di_app/di/NetworkModule.kt
@@ -1,10 +1,15 @@
 package uz.muhammad.hilt_di_app.di
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import uz.muhammad.hilt_di_app.BuildConfig
 import javax.inject.Singleton
 
@@ -21,4 +26,20 @@ object NetworkModule {
                 HttpLoggingInterceptor.Level.NONE
             }
         }
+
+    @Provides @Singleton
+    fun provideOkHttpClient(logging: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient.Builder().addInterceptor(logging).build()
+
+    @Provides @Singleton
+    fun provideMoshi(): Moshi =
+        Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    @Provides @Singleton
+    fun provideRetrofit(client: OkHttpClient, moshi: Moshi): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("https://talkmate.uz")
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(client)
+            .build()
 }
