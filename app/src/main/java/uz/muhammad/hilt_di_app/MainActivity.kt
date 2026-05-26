b4ad55b661bee945fd8dbd716851e2854e31f5b6
Muhammad Khasanboev <m.khasanboev@tadi.uz>
2026-05-26 15:08:10 +0500

    ui added to mainactivity

--- app/src/main/java/uz/muhammad/hilt_di_app/MainActivity.kt
+++ app/src/main/java/uz/muhammad/hilt_di_app/MainActivity.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.AndroidEntryPoint
+import jakarta.inject.Inject
+import uz.muhammad.hilt_di_app.data.repository.posts.PostsRepository
+import uz.muhammad.hilt_di_app.ui.posts.ui.PostsScreen
 import uz.muhammad.hilt_di_app.ui.theme.HiltDIappTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @Inject
+    lateinit var postsRepository: PostsRepository
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             HiltDIappTheme {
-
+                PostsScreen()
             }
         }
     }
