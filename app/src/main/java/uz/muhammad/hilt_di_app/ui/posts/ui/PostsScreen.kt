b9ea3efca9322938321d1e12433453d91bbcf02f
Muhammad Khasanboev <m.khasanboev@tadi.uz>
2026-05-26 14:54:55 +0500

    posts screen ui

---
+++ app/src/main/java/uz/muhammad/hilt_di_app/ui/posts/ui/PostsScreen.kt
@@ -0,0 +1,30 @@
+package uz.muhammad.hilt_di_app.ui.posts.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PostsScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(25.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+
+        Button(
+            onClick = {}
+        ){
+            Text("Get post")
+        }
+    }
+}
